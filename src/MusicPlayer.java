7f42e358a5ef779d80e12f02561049fa09ecace4
Rituja Biswas <147023153+Rituja-Biswas@users.noreply.github.com>
2024-05-23 12:04:58 +0530

    MusicPlayer.java

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -6,13 +6,13 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class MusicPlayer extends PlaybackListener {
-    // this will be used to update isPaused more synchronously
+    // update isPaused synchronously
     private static final Object playSignal = new Object();
 
-    // need reference so that we can update the gui in this class
+    // update gui in this class
     private MusicPlayerGUI musicPlayerGUI;
 
-    // we will need a way to store our song's details, so we will be creating a song class
+    // store song's details, creating a song class
     private Song currentSong;
     public Song getCurrentSong(){
         return currentSong;
@@ -20,27 +20,24 @@ public class MusicPlayer extends PlaybackListener {
 
     private ArrayList<Song> playlist;
 
-    // we will need to keep track the index we are in the playlist
+    // keep track the index in the playlist
     private int currentPlaylistIndex;
 
-    // use JLayer library to create an AdvancedPlayer obj which will handle playing the music
     private AdvancedPlayer advancedPlayer;
 
-    // pause boolean flag used to indicate whether the player has been paused
     private boolean isPaused;
 
-    // boolean flag used to tell when the song has finished
     private boolean songFinished;
 
     private boolean pressedNext, pressedPrev;
 
-    // stores in teh last frame when the playback is finished (used for pausing and resuming)
+    // used for pausing and resuming
     private int currentFrame;
     public void setCurrentFrame(int frame){
         currentFrame = frame;
     }
 
-    // track how many milliseconds has passed since playing the song (used for updating the slider)
+    // used for updating the slider
     private int currentTimeInMilli;
     public void setCurrentTimeInMilli(int timeInMilli){
         currentTimeInMilli = timeInMilli;
@@ -55,11 +52,11 @@ public class MusicPlayer extends PlaybackListener {
         currentSong = song;
         playlist = null;
 
-        // stop the song if possible
+        // stop the song
         if(!songFinished)
             stopSong();
 
-        // play the current song if not null
+        // play the current song
         if(currentSong != null){
             // reset frame
             currentFrame = 0;
@@ -85,7 +82,7 @@ public class MusicPlayer extends PlaybackListener {
             // reach each line from the text file and store the text into the songPath variable
             String songPath;
             while((songPath = bufferedReader.readLine()) != null){
-                // create song object based on song path
+                // song object based on song path
                 Song song = new Song(songPath);
 
                 // add to playlist array list
@@ -100,7 +97,7 @@ public class MusicPlayer extends PlaybackListener {
             musicPlayerGUI.setPlaybackSliderValue(0);
             currentTimeInMilli = 0;
 
-            // update current song to the first song in the playlist
+            // update current song to first song in playlist
             currentSong = playlist.get(0);
 
             // start from the beginning frame
@@ -135,15 +132,15 @@ public class MusicPlayer extends PlaybackListener {
     }
 
     public void nextSong(){
-        // no need to go to the next song if there is no playlist
+        // no need to go to the next song
         if(playlist == null) return;
 
-        // check to see if we have reached the end of the playlist, if so then don't do anything
+        // check to see if reached the end of the playlist
         if(currentPlaylistIndex + 1 > playlist.size() - 1) return;
 
         pressedNext = true;
 
-        // stop the song if possible
+        // stop the song
         if(!songFinished)
             stopSong();
 
@@ -164,20 +161,19 @@ public class MusicPlayer extends PlaybackListener {
         musicPlayerGUI.updateSongTitleAndArtist(currentSong);
         musicPlayerGUI.updatePlaybackSlider(currentSong);
 
-        // play the song
         playCurrentSong();
     }
 
     public void prevSong(){
-        // no need to go to the next song if there is no playlist
+        // if there is no playlist
         if(playlist == null) return;
 
-        // check to see if we can go to the previous song
+        // if we can go to the previous song
         if(currentPlaylistIndex - 1 < 0) return;
 
         pressedPrev = true;
 
-        // stop the song if possible
+        // stop the song
         if(!songFinished)
             stopSong();
 
@@ -236,7 +232,7 @@ public class MusicPlayer extends PlaybackListener {
                             // update flag
                             isPaused = false;
 
-                            // notify the other thread to continue (makes sure that isPaused is updated to false properly)
+                            // notify the other thread to continue
                             playSignal.notify();
                         }
 
@@ -253,14 +249,14 @@ public class MusicPlayer extends PlaybackListener {
         }).start();
     }
 
-    // create a thread that will handle updating the slider
+    // thread that will handle updating the slider
     private void startPlaybackSliderThread(){
         new Thread(new Runnable() {
             @Override
             public void run() {
                 if(isPaused){
                     try{
-                        // wait till it gets notified by other thread to continue
+                        // wait till other thread to continue
                         // makes sure that isPaused boolean flag updates to false before continuing
                         synchronized(playSignal){
                             playSignal.wait();
@@ -281,7 +277,7 @@ public class MusicPlayer extends PlaybackListener {
                         // update gui
                         musicPlayerGUI.setPlaybackSliderValue(calculatedFrame);
 
-                        // mimic 1 millisecond using thread.sleep
+                        // mimic 1 millisecond
                         Thread.sleep(1);
                     }catch(Exception e){
                         e.printStackTrace();
@@ -293,7 +289,7 @@ public class MusicPlayer extends PlaybackListener {
 
     @Override
     public void playbackStarted(PlaybackEvent evt) {
-        // this method gets called in the beginning of the song
+       
         System.out.println("Playback Started");
         songFinished = false;
         pressedNext = false;
@@ -302,12 +298,12 @@ public class MusicPlayer extends PlaybackListener {
 
     @Override
     public void playbackFinished(PlaybackEvent evt) {
-        // this method gets called when the song finishes or if the player gets closed
+        
         System.out.println("Playback Finished");
         if(isPaused){
             currentFrame += (int) ((double) evt.getFrame() * currentSong.getFrameRatePerMilliseconds());
         }else{
-            // if the user pressed next or prev we don't need to execute the rest of the code
+            // don't need to execute the rest of the code
             if(pressedNext || pressedPrev) return;
 
             // when the song ends
